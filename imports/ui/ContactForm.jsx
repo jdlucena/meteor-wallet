3930f2699a6afc081821f12c755078b251aa70b7
Jefferson Lucena <jeffersondelucena@hotmail.com>
2023-02-18 00:36:52 -0300

    lesson tailwind commit

--- imports/ui/ContactForm.jsx
+++ imports/ui/ContactForm.jsx
@@ -5,32 +5,66 @@ export const ContactForm = () => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-    const [imageURL, setImageURL] = useState("");
+    const [imageUrl, setimageUrl] = useState("");
 
     const saveContact = () => {
-        ContactsCollection.insert({name, email, imageURL});
+        ContactsCollection.insert({name, email, imageUrl});
         setName("");
         setEmail("");
-        setImageURL("");
+        setimageUrl("");
     }
 
     return (
-        <form>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input id='name' value={name} onChange={(e) => setName(e.target.value)} type="text" />
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input id='email' value={email} onChange={(e) => setEmail(e.target.value)} type="email" />
-            </div>
-            <div>
-                <label htmlFor="imageURL">image URL</label>
-                <input id='imageURL' value={imageURL} onChange={(e) => setImageURL(e.target.value)} type="text" />
-            </div>
-            <div>
-                <button type="button" onClick={saveContact}>Save Contact</button>
-            </div>
-        </form>
+        <form className="mt-6">
+      <div className="grid grid-cols-6 gap-6">
+        <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+          <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700">
+            Image URL
+          </label>
+          <input
+            type="text"
+            id="imageUrl"
+            value={imageUrl}
+            onChange={(e) => setimageUrl(e.target.value)}
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+      </div>
+      <div className="px-2 py-3 text-right">
+        <button
+          type="button"
+          onClick={saveContact}
+          className="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+        >
+          Save Contact
+        </button>
+      </div>
+    </form>
     )
 }
--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -9,12 +9,26 @@ export const ContactList = () => {
     
     return (
         <div>
-            <h3>Contact List</h3>
-            <ul>
-                {contacts.map(contact => (
-                    <li key={contact._id}>{contact.name} - {contact.email}</li>
-                ))}
+          <div className="mt-10">
+            <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+              Contact List
+            </h3>
+            <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+              {contacts.map((person, personIdx) => (
+                <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
+                  <div className="min-w-0 flex-1 flex items-center space-x-3">
+                    <div className="flex-shrink-0">
+                      <img className="h-10 w-10 rounded-full" src="/images/profile-picture-5.jpg" alt="" />
+                    </div>
+                    <div className="min-w-0 flex-1">
+                      <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
+                      <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
             </ul>
+          </div>
         </div>
-    )
+      )
 }
